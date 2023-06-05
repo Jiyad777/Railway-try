c376fec00419f2064e58e90a8215d8c2bd03248a
ᴍʀ ʙᴜɢ <110754631+Mr-BUG-TG@users.noreply.github.com>
2023-06-05 14:53:17 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -6,10 +6,8 @@ from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
-lock = asyncio.Lock()
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
@@ -24,13 +22,13 @@ async def verupikkals(bot, message):
     failed =0
 
     success = 0
-    temp = 0 # temp var
-    async with lock:
-        async for user in users:
-            if temp == 26: # after 25 msgs
-                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}\n\nSleeping for 2 seconds !") # editing msg
-                await asyncio.sleep(2) # sleeping for 2 seconds 😴
-                temp = 0 # resetting temp 
+    #temp = 0 # temp var
+    #async with lock:
+    async for user in users:
+            #if temp == 26: # after 25 msgs
+                #await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}\n\nSleeping for 2 seconds !") # editing msg
+                #await asyncio.sleep(2) # sleeping for 2 seconds 😴
+                #temp = 0 # resetting temp 
             pti, sh = await broadcast_messages(int(user['id']), b_msg)
             if pti:
                 success += 1
@@ -42,8 +40,8 @@ async def verupikkals(bot, message):
                 elif sh == "Error":
                     failed += 1
             done += 1
-            temp +=1 # incrementing with 1
-            if not done % 20:
-                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            #temp +=1 # incrementing with 1
+            #if not done % 20:
+                #await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
